e1ec07641d8f9931044a1a31d42ba1416d802f63
Joe George <joseph.george@cardinalhealth.com>
2017-08-01 13:49:48 -0400

    Need to pass values in

--- install-aws-service-broker/tasks/configure-json/task.sh
+++ install-aws-service-broker/tasks/configure-json/task.sh
@@ -17,14 +17,38 @@ OPS_MGR_HOST="opsman.$pcf_ert_domain"
 OPS_MGR_USR="$pcf_opsman_admin"
 OPS_MGR_PWD="$pcf_opsman_admin_passwd"
 
+CWD=$(pwd)
+pushd $CWD
+  cd pcf-pipelines/install-aws-service-broker/terraform/
+  cp $CWD/terraform-state/terraform.tfstate .
+
+  while read -r line
+  do
+    `echo "$line" | awk '{print "export "$1"="$3}'`
+  done < <(terraform output -state *.tfstate)
+popd
+
+pushd $CWD
+  cd pcf-pipelines/install-pcf/aws/terraform/
+  cp $CWD/pcf-terraform-state/terraform.tfstate .
+
+  while read -r line
+  do
+    `echo "$line" | awk '{print "export "$1"="$3}'`
+  done < <(terraform output -state *.tfstate)
+popd
+
 sed -i \
   -e "s/{{pcf_az_1}}/${pcf_az_1}/g" \
   -e "s/{{pcf_az_2}}/${pcf_az_2}/g" \
   -e "s/{{pcf_az_3}}/${pcf_az_3}/g" \
   -e "s/{{pcf_ert_domain}}/${pcf_ert_domain}/g" \
   -e "s/{{terraform_prefix}}/${terraform_prefix}/g" \
+  -e "s/{{db_host}}/${db_host}/g" \
   -e "s/{{db_aws_service_broker_username}}/${db_aws_service_broker_username}/g" \
   -e "s/{{db_aws_service_broker_password}}/${db_aws_service_broker_password}/g" \
+  -e "s/{{keyid}}/${broker_access_key_id}/g" \
+  -e "s/{{secretkey}}/${broker_secret_access_key}/g" \
   ${json_file}
 
 db_creds=(
