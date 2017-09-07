f08a69371c981a361bd20ee29cd542380792035f
Joe George <joseph.george@cardinalhealth.com>
2017-09-07 15:44:27 -0400

    Undo the bucket name change for the moment.

--- install-pcf/aws/terraform/s3.tf
+++ install-pcf/aws/terraform/s3.tf
@@ -1,10 +1,10 @@
-resource "aws_s3_bucket" "bosh" {
-    bucket = "${var.prefix}-bosh"
+resource "aws_s3_bucket" "pcf-bosh" {
+    bucket = "${var.prefix}-pcf-bosh"
     acl = "private"
     force_destroy= true
 
     tags {
-        Name = "${var.prefix}-bosh"
+        Name = "${var.prefix}-pcf-bosh"
         Environment = "${var.prefix}"
 
         "fuse:terraform" = "pivotal-sb1"
@@ -13,13 +13,13 @@ resource "aws_s3_bucket" "bosh" {
     }
 }
 
-resource "aws_s3_bucket" "buildpacks" {
-    bucket = "${var.prefix}-buildpacks"
+resource "aws_s3_bucket" "pcf-buildpacks" {
+    bucket = "${var.prefix}-pcf-buildpacks"
     acl = "private"
     force_destroy= true
 
     tags {
-        Name = "${var.prefix}-buildpacks"
+        Name = "${var.prefix}-pcf-buildpacks"
         Environment = "${var.prefix}"
 
         "fuse:terraform" = "pivotal-sb1"
@@ -28,13 +28,13 @@ resource "aws_s3_bucket" "buildpacks" {
     }
 }
 
-resource "aws_s3_bucket" "droplets" {
-    bucket = "${var.prefix}-droplets"
+resource "aws_s3_bucket" "pcf-droplets" {
+    bucket = "${var.prefix}-pcf-droplets"
     acl = "private"
     force_destroy= true
 
     tags {
-        Name = "${var.prefix}-droplets"
+        Name = "${var.prefix}-pcf-droplets"
         Environment = "${var.prefix}"
 
         "fuse:terraform" = "pivotal-sb1"
@@ -43,13 +43,13 @@ resource "aws_s3_bucket" "droplets" {
     }
 }
 
-resource "aws_s3_bucket" "packages" {
-    bucket = "${var.prefix}-packages"
+resource "aws_s3_bucket" "pcf-packages" {
+    bucket = "${var.prefix}-pcf-packages"
     acl = "private"
     force_destroy= true
 
     tags {
-        Name = "${var.prefix}-packages"
+        Name = "${var.prefix}-pcf-packages"
         Environment = "${var.prefix}"
 
         "fuse:terraform" = "pivotal-sb1"
@@ -58,13 +58,13 @@ resource "aws_s3_bucket" "packages" {
     }
 }
 
-resource "aws_s3_bucket" "resources" {
-    bucket = "${var.prefix}-resources"
+resource "aws_s3_bucket" "pcf-resources" {
+    bucket = "${var.prefix}-pcf-resources"
     acl = "private"
     force_destroy= true
 
     tags {
-        Name = "${var.prefix}-resources"
+        Name = "${var.prefix}-pcf-resources"
         Environment = "${var.prefix}"
 
         "fuse:terraform" = "pivotal-sb1"
